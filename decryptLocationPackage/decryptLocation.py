ed103de9ca355c5d4aa562f8e9df911516012f74
Scham <Scham@DESKTOP-3IC4JBC.wifi.uc.edu>
2023-11-28 10:20:05 -0500

    third hamiltsw commit

--- decryptLocationPackage/decryptLocation.py
+++ decryptLocationPackage/decryptLocation.py
@@ -1,3 +1,13 @@
+# Name: Stewary Hamilton (Holman)
+# email: hamiltsw@mail.uc.edu
+# Assignment Title: Assignment Final Project
+# Due Date: 20231207
+# Course: IS 4010
+# Semester/Year: Fall 2023
+# Brief Description: Decrypts the location clue for a team
+# Citations: 
+# Anything else that's relevant: 
+
 import json
 
 def decryptLocation(team):
@@ -6,6 +16,7 @@ def decryptLocation(team):
     @param team: the team that the clue belongs to
     @return: decrypted text containing the location clue
     '''
+    
     with open('EncryptedGroupHints Fall 2023 Section 001.json') as f:
         encrypted = json.load(f)[team]
     
