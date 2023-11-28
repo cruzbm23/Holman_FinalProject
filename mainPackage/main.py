ca35073c94b71a7718dc003bbce6a609b5a984f5
abbybuysse <abbybuysse@localhost>
2023-11-28 10:57:06 -0500

    buyssead

---
+++ mainPackage/main.py
@@ -0,0 +1,19 @@
+# Name: Abby Buysse (Holman)
+# email: buyssead@mail.uc.edu
+# Assignment Title: Assignment Final Project
+# Due Date: 20231207
+# Course: IS 4010
+# Semester/Year: Fall 2023
+# Brief Description: Envokes functions
+# Citations:
+# Anything else that's relevant:
+if __name__ == "__main__":
+   from decryptLocationPackage.decryptLocation import *
+   from decryptMoviePackage.decryptMovie import *
+   from displayPhotoPackage.displayPhoto import *
+  
+   decryptLocation()
+  
+   decode()
+  
+   displayPhoto()
