3eb5117259f8a4fa37e3dce9ade9e59a950bdff1
Scham <Scham@DESKTOP-3IC4JBC.wifi.uc.edu>
2023-11-28 10:14:40 -0500

    second hamiltsw commit

--- decryptLocationPackage/decryptLocation.py
+++ decryptLocationPackage/decryptLocation.py
@@ -1,6 +1,11 @@
 import json
 
 def decryptLocation(team):
+    '''
+    Decrypts the location clue for a team
+    @param team: the team that the clue belongs to
+    @return: decrypted text containing the location clue
+    '''
     with open('EncryptedGroupHints Fall 2023 Section 001.json') as f:
         encrypted = json.load(f)[team]
     
